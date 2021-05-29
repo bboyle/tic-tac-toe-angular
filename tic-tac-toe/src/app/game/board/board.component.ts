b5a278036a8685b6cce9dcfe62a79713711dfd07
Ben Boyle <benjamins.boyle@gmail.com>
2021-05-29 15:08:13 +1000

    basic gameplay
    - display buttons with grid numbers
    - click to set O/X, alternating player
    - "New Game" button
    
    TODO
    - detect game over state
    - prettier UI

--- tic-tac-toe/src/app/game/board/board.component.ts
+++ tic-tac-toe/src/app/game/board/board.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+// a tic tac toe board is an array of rows
+// each cell can be marked by player 1 or 2, or be null (not yet played)
+type TicTacToePlayer = 1 | 2;
+type TicTacToeValue = TicTacToePlayer | null;
+type TicTacToeBoard = TicTacToeValue[][];
 
 @Component({
   selector: 'tictac-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss']
 })
-export class BoardComponent implements OnInit {
+export class BoardComponent {
 
-  constructor() { }
+  // need to eventually protect this from changes via browser dev tools
+  grid: TicTacToeBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  ];
 
-  ngOnInit(): void {
+  player: TicTacToePlayer = 1;
+
+
+  newGame() {
+    this.player = 1;
+    this.grid.forEach(row => row.fill(null));
   }
 
+
+  play(row: number, column: number, player: TicTacToePlayer) {
+    if (!Array.isArray(this.grid[row]) || this.grid[row][column] !== null) return;
+    this.grid[row][column] = player;
+    this.player = this.player === 1 ? 2 : 1
+  }
 }
