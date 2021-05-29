ae9e31983ad40d97f4a130e86c3cfbb213a45dd5
Ben Boyle <benjamins.boyle@gmail.com>
2021-05-29 14:39:00 +1000

    basic board layout
    - fix app title
    - add game module, board component
    - set prefix to "tictac" (was generic "app")
    - initial UI for board (table with inner grid lines)

--- tic-tac-toe/src/app/app.component.ts
+++ tic-tac-toe/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'tictac-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tic-tac-toe';
+  title = 'Tic Tac Toe';
 }
